35f745c3a9f2fd031fb49e0fa003716bd0bbcb26
Onurcan <113804707+onrcn-tnrkl@users.noreply.github.com>
2024-06-23 11:58:46 +0300

    Add files via upload

---
+++ txtbox-xml/txtbox-xml/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace txtbox_xml
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 114);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "TextBox -> XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(932, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(321, 336);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "TextBox -> ListView";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ListView -> Xml";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Textbox -> Database";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(52, 252);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(165, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Database -> XML";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(310, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(602, 156);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(52, 310);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(165, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "XML -> Datagridview";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(310, 183);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(602, 174);
+            this.dataGridView2.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Kimlik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(49, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(49, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(49, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Adres";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 822);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ txtbox-xml/txtbox-xml/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data.SqlClient;
+
+namespace txtbox_xml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con= new SqlConnection("SQL DATA SOURCE BİLGİLERİNİZİ BURAYA GİRİN");
+        BindingSource bs = new BindingSource();
+
+        XmlDocument doc = new XmlDocument();
+        
+
+
+
+        public void listele()
+        {
+            SqlDataAdapter verial= new SqlDataAdapter("select * from human", con);
+            DataTable dt= new DataTable();
+            verial.Fill(dt);
+            bs.DataSource = dt;
+            dataGridView1.DataSource = bs;
+
+        }
+        public void list()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Columns.Add("Kimlik");
+            listView1.Columns.Add("Ad");
+            listView1.Columns.Add("Soyad");
+            listView1.Columns.Add("Adres");
+
+        }
+
+        public void clear()
+        {
+            var textboxs= Controls.OfType<TextBox>();
+            foreach(TextBox textbox in textboxs)
+            {
+                textbox.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Adım 1: Xml dosyasını oluştur ve load et
+            doc.Load(Application.StartupPath + "\\isim.xml");
+
+            //Adım 2: Oluşturulan dosya üzerinden yeni bir element oluştur ve özellik olarak (setAttribute()) primary değerini ata
+            XmlElement human = doc.CreateElement("human");
+            human.SetAttribute("kimlik", textBox1.Text);
+
+            //Adım 3: Gerekli node'ları oluşturulan dosya üzerinden oluştur ve innertextile değerleri ata. Element altına append et
+            XmlNode ad = doc.CreateNode(XmlNodeType.Element, "ad","");
+            ad.InnerText= textBox2.Text;
+            human.AppendChild(ad);
+
+            XmlNode soyad = doc.CreateNode(XmlNodeType.Element, "soyad", "");
+            soyad.InnerText = textBox3.Text;
+            human.AppendChild(soyad);
+
+            XmlNode adres = doc.CreateNode(XmlNodeType.Element, "adres", "");
+            adres.InnerText = textBox4.Text;
+            human.AppendChild(adres);
+
+            //Adım 4: Elementi dosya altına append et
+            doc.DocumentElement.AppendChild(human);
+
+            //Adım 5: dosyası save et
+            doc.Save(Application.StartupPath + "\\isim.xml");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list();
+            listele();
+            dataGridView1.ReadOnly= true;
+            dataGridView2.ReadOnly= true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListViewItem item=  new ListViewItem(textBox1.Text);
+            item.SubItems.Add(textBox2.Text);
+            item.SubItems.Add(textBox3.Text);
+            item.SubItems.Add(textBox4.Text);
+            
+            listView1.Items.Add(item);
+            clear();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                doc.Load(Application.StartupPath + "\\isim.xml");
+
+                foreach(ListViewItem item in listView1.Items)
+                {
+                    XmlElement human = doc.CreateElement("human");
+                    human.SetAttribute("kimlik", item.SubItems[0].Text);
+
+                    XmlNode ad = doc.CreateNode(XmlNodeType.Element, "ad", "");
+                    ad.InnerText = item.SubItems[1].Text;
+                    human.AppendChild(ad);
+
+                    XmlNode soyad = doc.CreateNode(XmlNodeType.Element, "soyad", "");
+                    soyad.InnerText = item.SubItems[2].Text;
+                    human.AppendChild(soyad);
+
+                    XmlNode adres = doc.CreateNode(XmlNodeType.Element, "adres", "");
+                    adres.InnerText = item.SubItems[3].Text;
+                    human.AppendChild(adres);
+
+                    doc.DocumentElement.AppendChild(human);
+                }
+                doc.Save(Application.StartupPath + "\\isim.xml");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into human(kimlik, ad, soyad, adres) values(@kimlik, @ad, @soyad, @adres)", con);
+            cmd.Parameters.AddWithValue("@kimlik", textBox1.Text);
+            cmd.Parameters.AddWithValue("@ad", textBox2.Text);
+            cmd.Parameters.AddWithValue("@soyad", textBox3.Text);
+            cmd.Parameters.AddWithValue("@adres", textBox4.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            clear();
+            listele();
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlCommand xmlcmd = new SqlCommand("select * from human", con);
+            con.Open();
+            SqlDataReader oku = xmlcmd.ExecuteReader();
+
+            doc.Load(@Application.StartupPath + "\\isim.xml");
+
+            while (oku.Read())
+            {
+                XmlElement human = doc.CreateElement("human");
+                human.SetAttribute("kimlik", oku["kimlik"].ToString());
+
+                string[] columns = { "ad", "soyad", "adres" }; 
+                foreach (string i in columns) 
+                {
+                    XmlNode node = doc.CreateNode(XmlNodeType.Element, i, "");
+                    node.InnerText = oku[i].ToString();
+                    human.AppendChild(node);
+                }
+                doc.DocumentElement.AppendChild(human);
+            }
+
+            con.Close();
+            doc.Save(@Application.StartupPath + "\\isim.xml");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Adım 1: Dosyayı yükle
+                doc.Load(@Application.StartupPath + "\\isim.xml");
+                //Adım 2: Dosya içeriğinin null olma olasılığını kontrol et
+                if (doc.DocumentElement != null)
+                {
+                    //Adım 3: Dataset oluştur ve datasetin readxml özelliği ile dosyayı oku
+                    //XmlNodeReader kullanma sebebimiz veriyi şerit benzeri bir yapıda okumak içindir. Yani dizi şeklinde
+                    DataSet ds = new DataSet();
+                    ds.ReadXml(new XmlNodeReader(doc));
+                    //Şuan dataset içerisinde xml dosyasındaki tablomuz var bunu verisetinden veritablosuna aktarmamız gerekiyor
+
+                    //Adım 4: Tablomuzun kolon isimlerini içeren bir dizi oluştur
+                    string[] columnorder = { "kimlik", "ad", "soyad", "adres"};
+
+                    //Adım 5: Datatable oluştur ve foreach ile dizideki okolon isimlerini datatable ın kolonlarına ekle
+                    DataTable dt = new DataTable();
+                    foreach (var columnname in columnorder)
+                    {
+                        dt.Columns.Add(columnname, typeof(string));
+                        //Columns.Add() methodu 2 parametre alır birincisi veri, 
+                    }
+
+                    //Adım 6: Dataset içerisindeki tablomuzu yeni oluşturduğumuz datatable ın satırlarına aktarıyoruz
+                    //Tables[0] dememizin sebebi xml dosyamızın içinde şuan tek tablo var
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        //Yeni bir row oluşturup ona verilerimizin bulunduğu tablodan newrow diyerek atama yapıyoruz
+                        DataRow drw = dt.NewRow();
+                        foreach (var columnname in columnorder)
+                        {   //drw yeni oluşturduğumuz datarowu temsil ederken row dataset içindeki tablomuzdaki rowu temsil eder
+                            //Temel olarak yaptığı şey verilerin bulunduğu satırdaki verileri yeni oluşturduğumuz satırlara kopyalamaktır.
+                            drw[columnname] = row[columnname];
+                        }
+                        //kopyalamalar bitince satırı datatable'a ekliyoruz
+                        dt.Rows.Add(drw);
+                    }
+
+                    dataGridView2.DataSource = dt;
+
+                    for (int i = 0; i < dataGridView2.Columns.Count; i++)
+                    {
+                        dataGridView2.Columns[i].HeaderText = dt.Columns[i].ColumnName;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
